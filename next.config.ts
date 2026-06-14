16ccd1b8480f00ef652d0c29035bea9db307d8a2
BalaciSofia <klara.balaci@stud.ubbcluj.ro>
2026-06-15 00:59:15 +0300

    deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/pixel-crochet-app",
+  assetPrefix: "/pixel-crochet-app/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
